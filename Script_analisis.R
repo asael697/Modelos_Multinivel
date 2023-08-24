2bc8bf2c99b29bba01f15313676af2dd75199a71
Eduardo Canales <120626994+Canales0396@users.noreply.github.com>
2023-08-24 11:36:20 -0600

    Estadisticos Descriptivos

--- Script_analisis.R
+++ Script_analisis.R
@@ -5,6 +5,7 @@ library(posterior)
 library(loo)
 library(xtable)
 library(ggplot2)
+library(ggthemes)
 library(cowplot)
 
 # ignora esta funcion
@@ -30,7 +31,7 @@ sm1 <- cmdstan_model("Stancodes/multi_level.stan")
 sm2 <- cmdstan_model("Stancodes/skew_normal.stan")
 
 # La lista de datos que Stan necesita para hacer mcmc
-d1 = list(n = length(LogGFN), J = 7, group = gl, y = LogGFN)
+d1 = list(n = length(LogGFN), J = 6, group = gl, y = LogGFN)
 
 # mcmc para modelo multinivel
 fit1 <- sm1$sample(data = d1, chains = 4, parallel_chains = 4, refresh = 500)
@@ -48,7 +49,7 @@ summarize_draws(fv1)
 
 # graficos de las posteriors multinivel
 g1 = mcmc_combo(fv1[,1:4],gg_theme = theme(legend.position = "none"))
-g2 = mcmc_combo(fv1[,5:8])
+g2 = mcmc_combo(fv1[,5:7])
 cowplot::plot_grid(g1,g2,ncol = 2,rel_widths = c(1, 1.2))
 
 
