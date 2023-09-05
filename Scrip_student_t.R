22172c467b8ec71f3223ecf6b67ed0df14c382c3
Eduardo Canales <120626994+Canales0396@users.noreply.github.com>
2023-09-04 22:45:54 -0600

    Script student t

---
+++ Scrip_student_t.R
@@ -0,0 +1,34 @@
+library(cmdstanr)
+library(bayesplot)
+library(loo)
+library(rstan)
+library(posterior)
+library(xtable)
+library(ggplot2)
+library(cowplot)
+library(ggthemes)
+
+# Compilar el codigo Stan del modelo student
+sm3 <- cmdstan_model("Stancodes/multi_level_student_t.stan")
+
+# mcmc para multinivel stundent_t
+fit3 <- sm3$sample(data = d1, chains = 4, parallel_chains = 4,refresh = 500)
+
+# extraer las cadenas de las variables importantes multinivel student_t
+fv3 = fit3$draws(variables = c("mu_group","sigma"),format = "matrix")
+colnames(fv3) = c(levels(glevels),'sigma')
+
+# Resumen de la cadenas
+summarise_draws(fv3)
+
+# graficos de las posteriors multinivel student_t
+g1 = mcmc_combo(fv3[,1:4],gg_theme = theme(legend.position = "none"))
+g2 = mcmc_combo(fv3[,5:7])
+cowplot::plot_grid(g1,g2,ncol = 2,rel_widths = c(1.1, 1.2))
+
+# Leave one out modelo multinivel student_t
+ll3 = fit3$draws(variables = "log_lik",format = "matrix")
+loo3 = loo(ll3)
+#Comparacion de los modelos
+comp2 = loo_compare(loo1,loo2,loo3)
+xtable(print(comp2,simplify = FALSE, digits = 2))
--- Script_analisis.R
+++ Script_analisis.R
@@ -30,9 +30,6 @@ sm1 <- cmdstan_model("Stancodes/multi_level.stan")
 # Compilar el codigo Stan del modelo de Gomez
 sm2 <- cmdstan_model("Stancodes/skew_normal.stan")
 
-# Compilar el codigo Stan del modelo student
-sm3 <- cmdstan_model("Stancodes/multi_level_student_t.stan")
-
 # La lista de datos que Stan necesita para hacer mcmc
 d1 = list(n = length(LogGFN), J = 6, group = gl, y = LogGFN)
 
@@ -42,40 +39,21 @@ fit1 <- sm1$sample(data = d1, chains = 4, parallel_chains = 4, refresh = 500)
 # mcmc para modelo de Gomez
 fit2 <- sm2$sample(data = d1, chains = 4, parallel_chains = 4,refresh = 500)
 
-# mcmc para multinivel stundent_t
-fit3 <- sm3$sample(data = d1, chains = 4, parallel_chains = 4,refresh = 500)
-
 # extraer las cadenas de las variables importantes multinivel
 fv1 = fit1$draws(variables = c("mu_group","sigma"),format = "matrix")
 colnames(fv1) = c(levels(glevels),'sigma')
 
-# extraer las cadenas de las variables importantes multinivel student_t
-fv3 = fit3$draws(variables = c("mu_group","sigma"),format = "matrix")
-colnames(fv3) = c(levels(glevels),'sigma')
-
-
 # resumen de las cadenas
 summarize_draws(fv1)
-summarise_draws(fv3)
 
 # graficos de las posteriors multinivel
 g1 = mcmc_combo(fv1[,1:4],gg_theme = theme(legend.position = "none"))
 g2 = mcmc_combo(fv1[,5:7])
 cowplot::plot_grid(g1,g2,ncol = 2,rel_widths = c(1.1, 1.2))
 
-# graficos de las posteriors multinivel student_t
-g1 = mcmc_combo(fv3[,1:4],gg_theme = theme(legend.position = "none"))
-g2 = mcmc_combo(fv3[,5:7])
-cowplot::plot_grid(g1,g2,ncol = 2,rel_widths = c(1.1, 1.2))
-
-
 # Leave one out modelo multinivel
 ll1 = fit1$draws(variables = "log_lik",format = "matrix")
 loo1 = loo(ll1)
-
-# Leave one out modelo multinivel student_t
-ll3 = fit3$draws(variables = "log_lik",format = "matrix")
-loo3 = loo(ll3)
 
 # Leave one out modelo Gomez
 ll2 = fit2$draws(variables = "log_lik",format = "matrix")
@@ -84,7 +62,6 @@ loo2 = loo(ll2)
 comp = loo_compare(loo1,loo2)
 xtable(print(comp,simplify = FALSE, digits = 2))
 
-comp2 = loo_compare(loo1,loo2,loo3)
-xtable(print(comp2,simplify = FALSE, digits = 2))
+
 
 
