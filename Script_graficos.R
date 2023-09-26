c9b0355866f361a851cf6c37ed8c2f29cbecfe04
Eduardo Canales <120626994+Canales0396@users.noreply.github.com>
2023-09-25 20:13:26 -0600

    Directorios
    correccion de directorio

--- Script_graficos.R
+++ Script_graficos.R
@@ -4,7 +4,7 @@ library(tidyquant)
 library(ggdist)
 library(ggthemes)
 
-load("~/Documentos/Modelos_Multinivel/Datos/Datos2016.RData")
+load("~/Documents/Modelos_Multinivel/Datos/Datos2016.RData")
 
 ## Grafico de Caja del Gasto y Densidad
 EGYPV2016TNF2 %>% 
