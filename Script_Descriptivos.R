6534e37f27efc3b903354cd73bbe5c8c73baae6e
Eduardo Canales <120626994+Canales0396@users.noreply.github.com>
2023-10-16 21:13:06 -0600

    Actualice los datos descriptivos para el 2021
    Realice los calculos de las estasticas descriptivas para los datos 2021

--- Script_Descriptivos.R
+++ Script_Descriptivos.R
@@ -1,4 +1,5 @@
 library(xtable)
+library(moments)
 ## Conversión del Gasto Fin a escala Logarítmica
 GastoFinN = EGYPV2016TNF2$GastoFin
 glevels = factor(EGYPV2016TNF2$Zona1)
@@ -19,10 +20,12 @@ kurtosis(EGYPV2016TNF2$GastoFin,na.rm = TRUE)
 
 ## Resumen de los turisticas de cuantas noches pernotaron en el pais
 summary(EGYPV2016TNF2$P10_3NumNoch)
+summary(ECV2021NF$P10D)
+
 ## Base para identificar los turistas que gastaron arriba y menos de 700 dolares
 
 EGYP1<-subset(EGYPV2016TNF2,GastoFin > 700)
-EGYP2<-subset(EGYPV2016TNF2,GastoFin < 700)
+EGYP2<-subset(EGYPV2016TNF2,GastoFin <= 700)
 
 ## Estadística Descriptivas por Zonas
 resumen_zonas <- EGYPV2016TNF2 %>%
@@ -61,3 +64,71 @@ valores_atipicos <- GastoFinN[GastoFinN < lim_inf | GastoFinN > lim_sup]
 porcentaje_atipicos <- (length(valores_atipicos) / length(GastoFinN)) * 100
 
 cat("Porcentaje de valores atípicos del gasto:", porcentaje_atipicos, "%\n")
+
+
+
+#####################################################################################
+#               Datos para el 2021
+#####################################################################################
+
+### Calculo de las estadísticas descriptivas de la variables Gasto Fin
+### y por Zonas respectivamente
+
+summary(ECV2021NF$PGastoTotal)
+##Desviacion
+sd(ECV2021NF$PGastoTotal,na.rm = TRUE)
+## Asimetria
+skewness(ECV2021NF$PGastoTotal)
+## Curtosis
+kurtosis(ECV2021NF$PGastoTotal,na.rm = TRUE)
+
+
+## Resumen de los turisticas de cuantas noches pernotaron en el pais
+summary(ECV2021NF$P10D)
+
+## Base para identificar los turistas que gastaron arriba y menos de 700 dolares
+
+EGYP11<-subset(ECV2021NF,PGastoTotal > 700)
+EGYP22<-subset(ECV2021NF,PGastoTotal<=700)
+
+## Estadística Descriptivas por Zonas
+resumen_zonas <- ECV2021NF %>%
+  group_by(P11_Zona1) %>%
+  summarise(
+    q1 = quantile(PGastoTotal , 0.25,na.rm = TRUE),
+    Media = mean(PGastoTotal , na.rm = TRUE),
+    Mediana = median(PGastoTotal , na.rm = TRUE),
+    DesvEstandar = sd(PGastoTotal , na.rm = TRUE),
+    q3 = quantile(PGastoTotal , 0.75,na.rm = TRUE),
+    Minimo = min(PGastoTotal , na.rm = TRUE),
+    Maximo = max(PGastoTotal , na.rm = TRUE),
+    asimetria=skewness(PGastoTotal),
+    Curtosis=kurtosis(PGastoTotal ,na.rm=TRUE)
+  )
+print(resumen_zonas)
+
+##Codigo latex de la tabla
+xtable(print(resumen_zonas,simplify = FALSE, digits = 2))
+resumen_zonas1 <- ECV2021NF%>%
+  group_by(P11_Zona1) %>%
+  count()
+print(resumen_zonas1)
+##Codigo latex de la tabla
+xtable(print(resumen_zonas1,simplify = FALSE, digits = 2))
+## Porcentaje de valores atipicos
+Q1 <- quantile(ECV2021NF$PGastoTotal , 0.25,na.rm = TRUE)
+Q3 <- quantile(ECV2021NF$PGastoTotal , 0.75,na.rm = TRUE)
+IQR <- Q3 - Q1
+
+# Definir límites para detectar valores atípicos
+lim_inf <- Q1 - 1.5 * IQR
+lim_sup <- Q3 + 1.5 * IQR
+
+# Calcular porcentaje de valores atípicos
+valores_atipicos <- ECV2021NF$PGastoTotal[ECV2021NF$PGastoTotal< lim_inf | ECV2021NF$PGastoTotal> lim_sup]
+porcentaje_atipicos <- (length(valores_atipicos) / length(ECV2021NF$PGastoTotal)) * 100
+
+cat("Porcentaje de valores atípicos del gasto:", porcentaje_atipicos, "%\n")
+
+q4 = quantile(ECV2021NF$PGastoTotal, 0.87,na.rm = TRUE)
+q4
